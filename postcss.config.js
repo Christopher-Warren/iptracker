b44e4cb62bd26a09763c24bc68f8a87280c882e7
Christopher Warren <chrisalmith@gmail.com>
2020-09-15 12:18:57 -0400

    init
    initial build of ip tracker

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+// Specifies what POSTCSS plugins we want to use.
+// Autoprefixer adds vendor prefixes. e.g. moz
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
