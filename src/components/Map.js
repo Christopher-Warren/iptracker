b871a0e6fceaeefc230b1cbfc25823c7662e558e
Christopher Warren <chrisalmith@gmail.com>
2020-09-15 23:57:21 -0400

    init

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import React from "react";
 const Map = () => {
   return (
     <div className="w-full h-64 bg-black -z-10">
-      IM THE MAP, IM THE MAP, IM THE MAP...
+      IM THE MAP, IM THE MAP, IM THE MAP..
     </div>
   );
 };
